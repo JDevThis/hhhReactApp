f44872c2aec91c6646cfdb98d33b6419bcbfc622
JDevThis <152988655+JDevThis@users.noreply.github.com>
2024-01-24 07:28:17 +0000

    Update reg-form.tsx
    Type error: Type 'string' is not assignable to type 'number'.
    
    fixed

--- hhh-app/src/app/ui/reg-form.tsx
+++ hhh-app/src/app/ui/reg-form.tsx
@@ -138,7 +138,7 @@ export default function LoginForm() {
       <div className="col-span-full">
         <label htmlFor="business_about" className="block text-sm font-medium leading-6 text-gray-900 ">About Business</label>
         <div className="mt-2">
-          <textarea id="business_about" name="b_about" rows="3" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+          <textarea id="business_about" name="b_about" rows={3} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
         </div>
         <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about your business.</p>
       </div>
