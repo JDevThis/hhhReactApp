aa53a55e8b36562f47d21cab87cf4a150119bfd7
JDevThis <152988655+JDevThis@users.noreply.github.com>
2024-01-24 07:55:05 +0000

    Update reg-form.tsx

--- hhh-app/src/app/ui/reg-form.tsx
+++ hhh-app/src/app/ui/reg-form.tsx
@@ -14,7 +14,7 @@ export default function LoginForm() {
                     alt="HisHerHub Logo"
                 />
                 </div>
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up and register your business</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
