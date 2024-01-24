c5ab125d7af033718069f15df95cec315178d529
JDevThis <152988655+JDevThis@users.noreply.github.com>
2024-01-24 07:43:35 +0000

    Update searchAC.tsx

--- hhh-app/src/app/ui/searchAC.tsx
+++ hhh-app/src/app/ui/searchAC.tsx
@@ -4,8 +4,11 @@ import { Combobox, Transition } from '@headlessui/react'
 import { CheckIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 export default function SearchForm({ placeholder }: { placeholder: string }) {
-  
-const searchlists = [
+  interface SearchList {
+    name: string;
+    id: string;
+  }
+const searchlists: SearchList[] = [
   { name: "1-1 classes", id: "1-1"},
   { name: "Custom colouring", id: "cc"},
   { name: "Custom wigs", id: "cw"},
@@ -32,88 +35,97 @@ const searchlists = [
   { name: "Grooming", id: "groom"},
 ]
 
-  const [selected, setSelected] = useState(searchlists[0])
-  const [query, setQuery] = useState('')
+const [selected, setSelected] = useState(searchlists[0]);
+const [query, setQuery] = useState('');
 
-  const filteredSearchlists =
-    query === ''
-      ? searchlists
-      : searchlists.filter((searchlist) =>
-          searchlist.name
-            .toLowerCase()
-            .replace(/\s+/g, '')
-            .includes(query.toLowerCase().replace(/\s+/g, ''))
-        )
-  return (
-    <div className="relative col-span-full z-50">
-      <Combobox onChange={setSelected}>
-        <div className="relative mt-1">
-          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-            <Combobox.Input
-              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(searchlist) => searchlist.name}
-              onChange={(event) => setQuery(event.target.value)}
-              id='service'
-              name='service'
-              placeholder={placeholder}
+const filteredSearchlists =
+  query === ''
+    ? searchlists
+    : searchlists.filter((searchlist) =>
+        searchlist.name
+          .toLowerCase()
+          .replace(/\s+/g, '')
+          .includes(query.toLowerCase().replace(/\s+/g, ''))
+      );
+
+return (
+  <div className="relative col-span-full z-50">
+    <Combobox onChange={setSelected}>
+      <div className="relative mt-1">
+        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <Combobox.Input
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            onChange={(event) => setQuery(event.target.value)}
+            id="service"
+            name="service"
+            placeholder={placeholder}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <MagnifyingGlassIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
             />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <MagnifyingGlassIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </Combobox.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setQuery('')}
-          >
-            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {filteredSearchlists.length === 0 && query !== '' ? (
-                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-                  Nothing found.
-                </div>
-              ) : (
-                filteredSearchlists.map((searchlist) => (
-                  <Combobox.Option
-                    key={searchlist.id}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
-                      }`
-                    }
-                    value={searchlist}
-                  >
-                    {({ selected, active }) => (
-                      <>
+          </Combobox.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery('')}
+        >
+          <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+            {filteredSearchlists.length === 0 && query !== '' ? (
+              <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                Nothing found.
+              </div>
+            ) : (
+              filteredSearchlists.map((searchlist: SearchList) => (
+                <Combobox.Option
+                  key={searchlist.id}
+                  className={({ active }: { active: boolean }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                    }`
+                  }
+                  value={searchlist}
+                >
+                  {({
+                    selected,
+                    active,
+                  }: {
+                    selected: boolean;
+                    active: boolean;
+                  }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {searchlist.name}
+                      </span>
+                      {selected ? (
                         <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? 'text-white' : 'text-teal-600'
                           }`}
                         >
-                          {searchlist.name}
+                          <CheckIcon
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                          />
                         </span>
-                        {selected ? (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-teal-600'
-                            }`}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
-        </div>
-      </Combobox>
-    </div>
-  )
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
+      </div>
+    </Combobox>
+  </div>
+);
 }
